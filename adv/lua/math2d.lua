708a95c7dd812842a049f03b2bdc12dd5ecbc4b8
Shrike78 <luca.ferrara@gmail.com>
2026-03-23 15:45:11 +0100

    fixed math2d.lua calls

--- adv/lua/math2d.lua
+++ adv/lua/math2d.lua
@@ -38,10 +38,10 @@ end
 function M.doIntersect(p1,q1,p2,q2)
 	-- Find the four orientations needed for general and
 	-- special cases
-	o1 = orientation(p1, q1, p2)
-	o2 = orientation(p1, q1, q2)
-	o3 = orientation(p2, q2, p1)
-	o4 = orientation(p2, q2, q1)
+	o1 = M.orientation(p1, q1, p2)
+	o2 = M.orientation(p1, q1, q2)
+	o3 = M.orientation(p2, q2, p1)
+	o4 = M.orientation(p2, q2, q1)
 
 	-- gerenal case (without limite case)
 	if(o1 ~= o2 and o3 ~= o4) then
@@ -50,13 +50,13 @@ function M.doIntersect(p1,q1,p2,q2)
 
 	-- Special case
 	-- p1, q1 and p2 are colinear and p2 lies on segment p1q1
-	if (o1 == 0 and onSegment(p1, p2, q1)) then return true end
+	if (o1 == 0 and M.onSegment(p1, p2, q1)) then return true end
 	-- p1, q1 and p2 are colinear and q2 lies on segment p1q1
-	if (o2 == 0 and onSegment(p1, q2, q1)) then return true end
+	if (o2 == 0 and M.onSegment(p1, q2, q1)) then return true end
 	--  p2, q2 and p1 are colinear and p1 lies on segment p2q2
-	if (o3 == 0 and onSegment(p2, p1, q2)) then return true end
+	if (o3 == 0 and M.onSegment(p2, p1, q2)) then return true end
 	-- p2, q2 and q1 are colinear and q1 lies on segment p2q2
-	if (o4 == 0 and onSegment(p2, q1, q2)) then return true end
+	if (o4 == 0 and M.onSegment(p2, q1, q2)) then return true end
 	return false; -- Doesn't fall in any of the above cases
 end
 
@@ -78,12 +78,12 @@ function M.isInside(listPoints,p,h)
 		ip = (i)%(#listPoints)+1
 		-- Check if the line segment from 'p' to 'extreme' intersects
 		-- with the line segment from 'polygon[i]' to 'polygon[next]'
-		if (doIntersect(listPoints[i], listPoints[ip], p, extreme)) then
+		if (M.doIntersect(listPoints[i], listPoints[ip], p, extreme)) then
 			-- If the point 'p' is colinear with line segment 'i-ip',
 			-- then check if it lies on segment. If it lies, return true,
 			-- otherwise false
-			if (orientation(listPoints[i], p, listPoints[ip]) == 0) then
-				return onSegment(listPoints[i], p, listPoints[ip])
+			if (M.orientation(listPoints[i], p, listPoints[ip]) == 0) then
+				return M.onSegment(listPoints[i], p, listPoints[ip])
 			end
 			count = count+1
 		end
